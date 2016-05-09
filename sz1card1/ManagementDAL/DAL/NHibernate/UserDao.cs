4f070ba46be64fdb357e8897756118864c0e2c66
yangkai <1183727552@qq.com>
2016-05-10 00:03:35 +0800

    增加nhibernate

---
+++ sz1card1/ManagementDAL/DAL/NHibernate/UserDao.cs
@@ -0,0 +1,41 @@
+﻿using ManagementDAL.IDAL;
+using ManagementDataModel.User;
+using NHibernate;
+using NHibernate.Cfg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagementDAL.DAL.NHibernate
+{
+    public class UserDao : IUser
+    {
+        private ISessionFactory sessionFactory;
+
+        public UserDao()
+        {
+            var cfg = new Configuration().Configure("Config/MySql.cfg.xml");
+            sessionFactory = cfg.BuildSessionFactory();
+        }
+
+        public ManagementDataModel.Models.User GetUser(Guid guid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ManagementDataModel.Models.User GetUser(string account)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Get(Guid account)
+        {
+            using (ISession session = sessionFactory.OpenSession())
+            {
+                return session.Get<User>(account);
+            }
+        }
+    }
+}
---
+++ sz1card1/UnitTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using ManagementDAL.DAL.NHibernate;
+using ManagementDataModel.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserDao userDao = new UserDao();
+            User user = userDao.Get(new Guid("9E16018F-9774-4D83-A693-1D1C7F6C1E59"));
+            Console.ReadKey();
+        }
+    }
+}
